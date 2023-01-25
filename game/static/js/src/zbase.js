05bfde478e7074b4e66f0a6f5fae8b7f1e5f090e
嘉红 李 <1430871335@qq.com>
2023-01-25 20:33:00 +0800

    working menu

--- game/static/js/src/zbase.js
+++ game/static/js/src/zbase.js
@@ -1,5 +1,9 @@
 class AcGame {
     constructor(id) {
-        console.log("111");
+        console.log("222");
+        this.id = id;
+        this.$ac_game = $('#' + id);
+        this.menu = new AcGameMenu(this);
+        this.playground = new AcGamePlayground(this);
     }
 }
